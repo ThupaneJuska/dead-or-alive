4b47473333eeda9b6680b103959fbb8434d8e5dd
Edward <edwardthupane80@gmail.com>
2024-08-16 13:14:53 +0200

    first commit

---
+++ app/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    "name": "dev",
+    "properties": {
+        "production": false,
+        "ssdURL": "http://localhost:8081/api/",
+        "tenantName": "neutrinos-delivery",
+        "appName": "dead-or-alive",
+        "namespace": "com.neutrinos-delivery.dead-or-alive",
+        "useDefaultExceptionUI": true,
+        "isIDSEnabled": "false",
+        "webAppMountpoint": "web",
+        "NGFORAGE_MOBILE_DRIVER": "INDEXED_DB"
+    }
+}

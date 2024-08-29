54e1140bf91bb7d87310aa5811b927b460d846fe
Nicholas <lesetjamaluleka@gmail.com>
2024-08-29 15:42:41 +0200

    view and make payments done

--- app/src/app/components/Admin Dashboard/home.component.ts
+++ app/src/app/components/Admin Dashboard/home.component.ts
@@ -385,7 +385,9 @@ export class homeComponent implements AfterViewInit {
         this.sdService.getPathAndQParamsObj('/dashboard/view-payments');
       await this.__page_injector__
         .get(Router)
-        .navigate([this.sdService.formatPathWithParams(path, undefined)]);
+        .navigate([this.sdService.formatPathWithParams(path, undefined)], {
+          queryParams: Object.assign(qprm, ''),
+        });
       //appendnew_next_sd_WnahUlnrdMBkCHsS
       return bh;
     } catch (e) {
